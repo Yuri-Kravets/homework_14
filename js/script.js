6ee06f3dfbcd8667221bee5f251f677cb8399ac1
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-11-03 12:55:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-function padString(String, Number,String, Bolean ) {
-  
-  alert(String + Number + String + Bolean);
+function isValidNumber(arg) {
+  if (typeof arg === 'number' && !isNaN(arg)) {
+    return true;
+  }
+  return false;
 }
 
-padString('string',456,'dsafd',false);
+function padString(str, strLenght, symb, right = true) {
+  if (typeof str !== 'string') return 'string не string';
+  if (!isValidNumber(strLenght)) return 'strLenght не number';
+  if (typeof symb !== 'string' && symb.lenght !== 1) return 'symb не string';
+  if (typeof right !== 'boolean') return 'flag не указан';
+
+  let localString = '';
+
+  for (let i = str.lenght; i < strLenght; i++) {
+    localString += symb;
+  }
+  return right ? str + localString : localString + str;
+}
+console.log(padString('lexaim',10,'*'));
+console.log(padString('lexaim',23,'*', false));
+
